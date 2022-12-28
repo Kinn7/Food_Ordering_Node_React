1d34a0fb5fd8e1f60af2952dda0902abd42230cf
Kinn7 <kidus.berhanu.gh9353@gmail.com>
2022-12-28 14:04:17 +0300

    Display Category

--- frontend/src/components/Category.jsx
+++ frontend/src/components/Category.jsx
@@ -1,54 +1,26 @@
-import React , { useEffect } from "react";
+import React from "react";
 
 
-const Category = () => {
+const Category = (props) => {
     
-const [category , setCategory] = React.useState()
 
-useEffect(() => {
-    fetch("http://localhost:3000/api/category")
-    .then(res => res.json())
-    .then(data => console.log(data))
-},[])
-
-// const cards = category.map((c) => {
-//     <li className="w-100 ">
-//     <a href="#" className="nav-link px-0 text-dark">
-//       {" "}
-//       <span className="d-none d-sm-inline">{c.categoryName}</span> 
-//     </a>
-//   </li>
-// })
-
-
-// function cards(){
-//     <li className="w-100 ">
-//     <a href="#" className="nav-link px-0 text-dark">
-//       {" "}
-//       <span className="d-none d-sm-inline">{category}</span> 
-//     </a>
-//   </li>
-// }
-    return (
-    <ul  
+{/* <ul  
         className="collapse show nav flex-column  ps-4 "
         id="submenu1"
         data-bs-parent="#menu"
-      >
         
-        <li className="w-100 ">
-          <a href="#" className="nav-link px-0 text-dark">
-            {" "}
-            <span className="d-none d-sm-inline text-white">Pasta Corner</span> 
-          </a>
-        </li>
-        <li>
-          <a href="#" className="nav-link px-0 text-dark">
-            {" "}
-            <span className="d-none d-sm-inline text-white">Traditional</span> 
-          </a>
-        </li>
-      </ul>
+      > 
+         </ul>
+   
+    */}
+
+    return (
+      <li className="w-100 ps-5 " >
+       <a href="#" className="nav-link px-0 text-dark" >
+         <span className="d-none d-sm-inline text-white" >{props.categoryName}</span> 
+       </a>
+     </li>
+ 
     )
 }
 
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,130 +1,47 @@
-import React from "react"
+import React , { useEffect , useState } from "react";
 import Category from "./Category"
 
 const Sidebar = () => {
+
+  const [category , setCategory] = useState([{
+
+  }])
+
+  useEffect(() => {
+    console.log("use effect run")
+      fetch("http://localhost:3000/api/category")
+      .then(res => res.json())
+      .then(data => setCategory(data.category))
+  },[])
+
+  const categories = category.map((c,i) => (
+    <Category key={i} categoryName={c.categoryName} />
+
+  ))
     return (
     <div className="container-fluid">
         <div className="row flex-nowrap">
             <div className="col-auto col-md-3 col-xl-2 px-sm-0 px-0   " style={{ backgroundColor : '#395144' }}>
                 <div className="d-flex flex-column align-items-center align-items-sm-start px-0 pt-2 text-white min-vh-100">
-                       
-                        <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu" >
-                        <span className=" d-none d-sm-inline  ps-3  " style={{ fontSize : 35 , color : '#E5E5CB'}}>ABC  Restaurant</span>
-          <li className="nav-item pt-5 " >
-              <span className="ms-1 d-none d-sm-inline   " style={{ fontStyle : "italic", fontWeight : "bold", fontSize : 32, fontFamily : 'pristina'  }}>Foods</span>
-          </li>
-          <li>
+                  <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu" >
+                    <span className=" d-none d-sm-inline  ps-3  " style={{ fontSize : 35 , color : '#E5E5CB'}}>ABC  Restaurant</span>
+                    <li className="nav-item pt-5 " >
+                      <span className="ms-1 d-none d-sm-inline   " style={{ fontStyle : "italic", fontWeight : "bold", fontSize : 32, fontFamily : 'pristina'  }}>Foods & Drinks</span>
+                      </li>
 
-          <Category />
 
-            {/* <ul style={{ backgroundColor : 'grey'  }}
-              className="collapse show nav flex-column ms-1"
-              id="submenu1"
-              data-bs-parent="#menu"
-            >
-              <li className="w-100 ">
-                <a href="#" className="nav-link px-0 text-dark">
-                  {" "}
-                  <span className="d-none d-sm-inline">Pasta Corner</span> 
-                </a>
-              </li>
-              <li>
-                <a href="#" className="nav-link px-0 text-dark">
-                  {" "}
-                  <span className="d-none d-sm-inline">Traditional</span> 
-                </a>
-              </li>
-            </ul> */}
-          </li>
-          {/* <li>
-            <a href="#" className="nav-link px-0 align-middle">
-              <i className="fs-4 bi-table" />{" "}
-              <span className="ms-1 d-none d-sm-inline">Orders</span>
-            </a>
-          </li> */}
-          <li>
-            {/* <a
-              href="#submenu2"
-              data-bs-toggle="collapse"
-              className="nav-link px-0 align-middle "
-            >
-              <i className="fs-4 bi-bootstrap" />{" "}
-              <span className="ms-1 d-none d-sm-inline">Bootstrap</span>
-            </a> */}
-            <ul
-              className="collapse nav flex-column ms-1"
-              id="submenu2"
-              data-bs-parent="#menu"
-            >
-              <li className="w-100">
-                <a href="#" className="nav-link px-0">
-                  {" "}
-                  <span className="d-none d-sm-inline">Item</span> 1
-                </a>
-              </li>
-              <li>
-                <a href="#" className="nav-link px-0">
-                  {" "}
-                  <span className="d-none d-sm-inline">Item</span> 2
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            {/* <a
-              href="#submenu3"
-              data-bs-toggle="collapse"
-              className="nav-link px-0 align-middle"
-            >
-              <i className="fs-4 bi-grid" />{" "}
-              <span className="ms-1 d-none d-sm-inline">Products</span>{" "}
-            </a> */}
-            {/* <ul
-              className="collapse nav flex-column ms-1"
-              id="submenu3"
-              data-bs-parent="#menu"
-            >
-              <li className="w-100">
-                <a href="#" className="nav-link px-0">
-                  {" "}
-                  <span className="d-none d-sm-inline">Product</span> 1
-                </a>
-              </li>
-              <li>
-                <a href="#" className="nav-link px-0">
-                  {" "}
-                  <span className="d-none d-sm-inline">Product</span> 2
-                </a>
-              </li>
-              <li>
-                <a href="#" className="nav-link px-0">
-                  {" "}
-                  <span className="d-none d-sm-inline">Product</span> 3
-                </a>
-              </li>
-              <li>
-                <a href="#" className="nav-link px-0">
-                  {" "}
-                  <span className="d-none d-sm-inline">Product</span> 4
-                </a>
-              </li>
-            </ul> */}
-          </li>
-          {/* <li>
-            <a href="#" className="nav-link px-0 align-middle">
-              <i className="fs-4 bi-people" />{" "}
-              <span className="ms-1 d-none d-sm-inline">Customers</span>{" "}
-            </a>
-          </li> */}
-        </ul>
-        <hr />
+                      {categories}
 
-      </div>
-    </div>
+
+                      </ul>
+                      <hr />
+                      </div>
+                      </div>
 
 
     <div className="col py-3">Content area...</div>
-    <div className="col-auto col-md-3 col-xl-2 px-sm-0 px-0   bg-warning bg-gradient" style={{ backgroundColor : 'rgb(66,33,11)' }}>
+    
+    {/* <div className="col-auto col-md-3 col-xl-2 px-sm-0 px-0   bg-warning bg-gradient" style={{ backgroundColor : 'rgb(66,33,11)' }}>
                 <div className="d-flex flex-column align-items-center align-items-sm-start px-0 pt-2 text-white min-vh-100">
                        
                         <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu" >
@@ -153,21 +70,9 @@ const Sidebar = () => {
               </li>
             </ul>
           </li>
-          {/* <li>
-            <a href="#" className="nav-link px-0 align-middle">
-              <i className="fs-4 bi-table" />{" "}
-              <span className="ms-1 d-none d-sm-inline">Orders</span>
-            </a>
-          </li> */}
+
           <li>
-            {/* <a
-              href="#submenu2"
-              data-bs-toggle="collapse"
-              className="nav-link px-0 align-middle "
-            >
-              <i className="fs-4 bi-bootstrap" />{" "}
-              <span className="ms-1 d-none d-sm-inline">Bootstrap</span>
-            </a> */}
+
             <ul
               className="collapse nav flex-column ms-1"
               id="submenu2"
@@ -187,57 +92,11 @@ const Sidebar = () => {
               </li>
             </ul>
           </li>
-          <li>
-            {/* <a
-              href="#submenu3"
-              data-bs-toggle="collapse"
-              className="nav-link px-0 align-middle"
-            >
-              <i className="fs-4 bi-grid" />{" "}
-              <span className="ms-1 d-none d-sm-inline">Products</span>{" "}
-            </a> */}
-            {/* <ul
-              className="collapse nav flex-column ms-1"
-              id="submenu3"
-              data-bs-parent="#menu"
-            >
-              <li className="w-100">
-                <a href="#" className="nav-link px-0">
-                  {" "}
-                  <span className="d-none d-sm-inline">Product</span> 1
-                </a>
-              </li>
-              <li>
-                <a href="#" className="nav-link px-0">
-                  {" "}
-                  <span className="d-none d-sm-inline">Product</span> 2
-                </a>
-              </li>
-              <li>
-                <a href="#" className="nav-link px-0">
-                  {" "}
-                  <span className="d-none d-sm-inline">Product</span> 3
-                </a>
-              </li>
-              <li>
-                <a href="#" className="nav-link px-0">
-                  {" "}
-                  <span className="d-none d-sm-inline">Product</span> 4
-                </a>
-              </li>
-            </ul> */}
-          </li>
-          {/* <li>
-            <a href="#" className="nav-link px-0 align-middle">
-              <i className="fs-4 bi-people" />{" "}
-              <span className="ms-1 d-none d-sm-inline">Customers</span>{" "}
-            </a>
-          </li> */}
         </ul>
         <hr />
 
       </div>
-    </div>
+    </div> */}
 
   </div>
 </div>
