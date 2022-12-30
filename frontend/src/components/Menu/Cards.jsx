8f6df6628b47938cc5ac13b7d9cccd9e734de40f
Kinn7 <kidus.berhanu.gh9353@gmail.com>
2022-12-30 23:42:22 +0300

    Finished with getting food item to list

--- frontend/src/components/Menu/Cards.jsx
+++ frontend/src/components/Menu/Cards.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 
-const Cards = () => {
+const Cards = (props) => {
+
+    //variable that holds menuItem(food lists from api) that iterates through the http://localhost:3000/api/menuitem/Category/${foodId} list api gotten from Menu Component passed through props
+    const menuItem = props.menuItem.map((c,i) => (
+        <div className="card bg-light mb-3" style={{maxWidth : "18rem"}} key={i}>
+            <div className="card-header">Header</div>
+                <div className="card-body">
+                    <h5 className="card-title" >{c.menuItemName}</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+        </div>
+        
+          ))
+          
     return(
-        <div className="col py-3">Content area...</div>
+        <div className="col py-3">Content area...
+
+        {menuItem}
+
+        </div>
     )
 }
 
--- frontend/src/components/Menu/Menu.jsx
+++ frontend/src/components/Menu/Menu.jsx
@@ -5,24 +5,57 @@ import Cart from "./Cart";
 
 
 const Menu = () => {
+    //category state to set in the sidebar
     const [category , setCategory] = useState([{
 
     }])
-  
+
+    //menuItem state to set it in the middle of the page to get food lists i.e get's list of food from categoryId
+    const [menuItem, setMenuItem] = useState([{}])
+
+    //foodId state to change re render cards component when clicking on the different categories
+    const [foodId, setFoodId] = useState(0)
+
+
+    //this function accepts id of category from Categories component and passed it to setFoodId to set foodId state
+    function getMenuItem(id){
+        setFoodId(id) //setting foodId will trigger useEffect and re-render the component of Cards.jx to get the food items
+    }
+
+
+    //this useEffect runs to get api of food items based on category id 
     useEffect(() => {
-      console.log("use effect run")
-        fetch("http://localhost:3000/api/category")
+       console.log('use effect run')
+        fetch(`http://localhost:3000/api/menuitem/Category/${foodId}`)
+        .then(res => res.json())
+        .then(data => setMenuItem(data.menuitem))
+    },[foodId])
+  
+    //this useEffect runs to get the category from api and use it in the sidebar 
+    useEffect(() => {
+      fetch("http://localhost:3000/api/category")
         .then(res => res.json())
         .then(data => setCategory(data.category))
     },[])
-  
 
+
+
+
+
+
+
+ 
     return (
         <div className="container-fluid">
             <div className="row flex-nowrap">
 
-                <Categories category = {category}/>
-                <Cards />
+                {/* Categories  component passes two properties category to iterate the categories from api and handleClick to get id of the category from the Categories component*/}
+                <Categories category = {category} handleClick={getMenuItem}/>
+              
+                {/* Cards component that passes property of menuItem to iterate the foodLists in the Cards component */}
+                <Cards menuItem = {menuItem} />
+
+                {/* Cart Component */}
                 <Cart />
                 
             </div>
