de38d2631219de5bff488697aa5e71266a0ae7c8
Kinn7 <kidus.berhanu.gh9353@gmail.com>
2023-01-05 11:25:37 +0300

    adjust cards style

--- frontend/src/components/Menu/Cards.jsx
+++ frontend/src/components/Menu/Cards.jsx
@@ -4,21 +4,23 @@ const Cards = (props) => {
 
     //variable that holds menuItem(food lists from api) that iterates through the http://localhost:3000/api/menuitem/Category/${foodId} list api gotten from Menu Component passed through props
     const menuItem = props.menuItem.map((c,i) => (
-        <div className="card bg-light mb-3" style={{maxWidth : "18rem"}} key={i}>
-            <div className="card-header">Header</div>
-                <div className="card-body">
-                    <h5 className="card-title" >{c.menuItemName}</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
+    <div className="card mx-5 p-0" key={i} style={{maxWidth : "18rem"}} onClick={() => props.handleClick(c.id,c.menuItemName) }>
+            <img src={c.pic} className="img-fluid card-img-top img-thumbnail" style={{ height : "15rem"}}/>
+        <div className="card-body">
+            <h5 className="card-title" >{c.menuItemName}</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#!" className="btn btn-primary">Button</a>
         </div>
+    </div>
+
         
           ))
-          
-    return(
-        <div className="col py-3">Content area...
+          return(
+          <div className="col py-3">Content area...
+            <div className="row">
+                {menuItem}
 
-        {menuItem}
-
+            </div>
         </div>
     )
 }
